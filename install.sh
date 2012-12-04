413441159120ec7c1ffe0bf3a9832ef7241ef66e
Brandon Brown <brandon@brandonbrown.io>
2012-12-03 17:01:26 -0800

    initial commit

---
+++ install.sh
@@ -0,0 +1,10 @@
+#! /bin/sh
+
+FOLDER='~/Dropbox/Apps'
+
+#alias zsh files
+ln -s $FOLDER/dotfiles/zsh      ~/.zsh
+ln -s $FOLDER/dotfiles/zsh/zshrc ~/.zshrc
+
+#alias git files
+ln -s $FOLDER/dotfiles/gitconfig ~/.gitconfig
