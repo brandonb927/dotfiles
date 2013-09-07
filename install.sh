62699943c2b21eb76c01a52cea8518303a85eead
Brandon Brown <brandon@brandonbrown.io>
2013-09-07 13:59:28 -0700

    Add custom theme and update files

--- install.sh
+++ install.sh
@@ -8,6 +8,9 @@ cp zsh/zshrc ~/.zshrc
 cp zsh/zprofile ~/.zprofile
 cat zsh/aliases >> ~/.zprofile
 
+# copy ZSH theme
+cp zsh/brandonbrown.zsh-theme ~/.oh-my-zsh/themes/brandonbrown.zsh-theme
+
 # copy git files
 cp gitconfig ~/.gitconfig
 cp gitignore ~/.gitignore
