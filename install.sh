79163a74cb1a318c16375cb87ad2ee25a8e76608
Brandon Brown <brandon@brandonbrown.io>
2013-09-07 15:12:16 -0700

    woops forgot to go up a directory

--- install.sh
+++ install.sh
@@ -4,13 +4,13 @@
 curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh
 
 # copy zsh files
-cp zsh/zshrc ~/.zshrc
-cp zsh/zprofile ~/.zprofile
-cat zsh/aliases >> ~/.zprofile
+cp zsh/.zshrc ~/.zshrc
+cp zsh/.zprofile ~/.zprofile
+cat zsh/.aliases >> ~/.zprofile
 
 # copy ZSH theme
 cp zsh/brandonbrown.zsh-theme ~/.oh-my-zsh/themes/brandonbrown.zsh-theme
 
 # copy git files
-cp gitconfig ~/.gitconfig
-cp gitignore ~/.gitignore
+cp .gitconfig ~/.gitconfig
+cp .gitignore ~/.gitignore
