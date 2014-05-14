4e0eb742e50f60c744f8b2d1b33e84e0b2a78444
Brandon Brown <brandon@brandonb.io>
2014-05-14 11:53:04 -0700

    Create brew-cask-installer.sh

---
+++ brew-cask-installer.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+echo 'Items not installed here (most likely from App store, or other sources):'
+echo '- Adobe Photoshop'
+echo '- Air Display Host'
+echo '- Air Video Server HD'
+echo '- Airmail (app store)'
+echo '- Caffeine (app store)'
+echo '- Cinch (app store)'
+echo '- Codekit2'
+echo '- Cronnix'
+echo '- Evernote (app store)'
+echo '- Instashare (app store)'
+echo '- iStats Menus'
+echo '- Mou '
+echo '- Pocket (app store)'
+echo '- Popcorn-time'
+echo '- Sublime Text 3'
+echo '- Tweetdeck (app store)'
+echo '- XCode (app store)'
+echo '- '
+
+brew cask install onepassword \
+  alfred appcleaner atom cakebrew daisydisk dropbox \
+  firefox flowdock fluid flux google-chrome iterm2 \
+  licecap onyx radiant-player steam transmission vlc \
+  
