b88fa8b6785282045514865e73f996eac100e68e
Brandon Brown <brandon@brandonb.io>
2014-08-19 16:55:42 -0700

    Add postgres

--- brew-cask-installer.sh
+++ brew-cask-installer.sh
@@ -6,16 +6,10 @@ echo '- Air Display Host'
 echo '- Air Video Server HD'
 echo '- Airmail (app store)'
 echo '- Android OS X SDK'
-echo '- Caffeine (app store)'
 echo '- Cinch (app store)'
-echo '- Codekit2'
-echo '- Cronnix'
 echo '- Evernote (app store)'
 echo '- Instashare (app store)'
 echo '- iStats Menus'
-echo '- Mou '
-echo '- Pocket (app store)'
-echo '- Popcorn-time'
 echo '- Sublime Text 3'
 echo '- Tweetdeck (app store)'
 echo '- XCode (app store)'
@@ -24,7 +18,7 @@ echo '- '
 brew cask install onepassword \
   alfred appcleaner atom cakebrew daisydisk dropbox \
   firefox flowdock fluid flux google-chrome iterm2 \
-  licecap onyx radiant-player steam send-to-kindle \
+  licecap onyx postgres radiant-player steam send-to-kindle \
   transmission vlc \
   
 brew cask cleanup
