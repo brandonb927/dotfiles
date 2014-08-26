1d9a935dee58d3b47459776ba4f9156129e9c2b3
Brandon Brown <brandon@brandonb.io>
2014-08-25 21:13:54 -0700

    Add skype and android-studio

--- brew-cask-installer.sh
+++ brew-cask-installer.sh
@@ -2,17 +2,16 @@
 
 echo 'Items not installed here (most likely from App store, or other sources):'
 echo '- Airmail (app store)'
-echo '- Android OS X SDK'
 echo '- Instashare (app store)'
 echo '- Tweetdeck (app store)'
 echo '- XCode (app store)'
 echo '- '
 
 brew cask install \
-  air-video-server-hd airserver airdisplay alfred appcleaner atom cakebrew cinch \
+  air-video-server-hd airserver airdisplay alfred android-studio appcleaner atom cakebrew cinch \
   daisydisk dropbox evernote \
   firefox flux google-chrome iterm2 istat-menus \
   licecap onepassword postgres steam send-to-kindle \
-  transmission vlc \
+  skype transmission vlc \
   
 brew cask cleanup
