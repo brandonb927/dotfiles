4df9f23b2fbafd781277d7ed1b57886cf8f3408e
Brandon Brown <brandon@brandonb.io>
2015-08-05 10:09:23 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -233,7 +233,7 @@ case $response in
     npm install -g nodemon
     npm install -g markdown-live
     npm install -g npm-check-updates npm-release
-    npm install -g peerflix public-ip
+    npm install -g peerflix perfschool public-ip
     npm install -g release-it resume-cli
     npm install -g standard surge svgo
     npm install -g uglifycss uglify-js underscore-cli
