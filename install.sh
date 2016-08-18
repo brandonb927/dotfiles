56a5a943104409fe3d94c5509a82f552a83e96c9
Brandon Brown <brandon@brandonb.io>
2016-08-17 18:58:25 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ green='\033[0;32m'
 cyan='\033[0;36m'
 red='\033[0;31m'
 purple='\033[0;35m'
-brown='\033[0;33m'
+brown='\033[0;33m'b
 gray='\033[0;37m'
 dark_gray='\033[1;30m'
 light_blue='\033[1;34m'
@@ -116,6 +116,7 @@ case $response in
       keybase
       ncdu
       normalize
+      phantomjs
       pv
       pyenv
       pyenv-virtualenv
