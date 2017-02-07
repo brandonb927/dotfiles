af92c855649779e425f04d43e0be8e307d94cbf1
Brandon Brown <brandon@brandonb.io>
2017-02-07 12:26:26 -0800

    Update brew-utilities.sh

--- install/brew-utilities.sh
+++ install/brew-utilities.sh
@@ -21,7 +21,7 @@ binaries=(
     git-extras
     git-fresh
     graphicsmagick
-    # heroku # This is an incredibly old version of the CLI for some reason (v3.4.x, current is v5.x)
+    heroku
     htop
     httpie
     hub
