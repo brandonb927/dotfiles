8467615d359ec6f49a5d07b681371bec438e6377
Brandon Brown <brandon@brandonb.io>
2017-03-25 12:27:19 -0700

    Update mas.sh

--- install/mas.sh
+++ install/mas.sh
@@ -12,7 +12,7 @@ e_arrow "Installing Mac App Store apps" $cyan
 mas install 497799835 # Xcode (8.2.1)
 mas install 425424353 # The Unarchiver (3.11.1)
 mas install 824183456 # Affinity Photo (1.5.2)
-mas install 692867256 # Simplenote (1.1.8)
+# mas install 692867256 # Simplenote (1.1.8)
 mas install 1091189122 # Bear (1.1)
 mas install 408981434 # iMovie (10.1.4)
 mas install 682658836 # GarageBand (10.1.6)
