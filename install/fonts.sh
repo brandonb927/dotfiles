8cc5132d6026961cfd2ea5447abb8c39fca483d0
Brandon Brown <brandon@brandonb.io>
2017-07-31 15:22:56 -0700

    Update fonts.sh

--- install/fonts.sh
+++ install/fonts.sh
@@ -5,7 +5,8 @@ source ./utils.sh
 e_header "Installing fonts"
 
 brew tap caskroom/fonts
-brew cask install font-awesome-terminal-fonts
+
+mkdir -p ~/.fonts
 
 curl https://raw.githubusercontent.com/qrpike/Web-Font-Load/master/install.sh | sh
 
@@ -14,8 +15,13 @@ unzip noto.zip -d noto
 cp noto/NotoMono-Regular.ttf /Library/Fonts/
 rm -r noto*
 
-wget https://github.com/powerline/fonts/archive/master.zip -O fonts.zip && unzip fonts.zip
+wget https://github.com/powerline/fonts/archive/master.zip -O powerline-fonts.zip && unzip powerline-fonts.zip
 ./fonts-master/install.sh
-rm -r fonts.zip fonts-master
+rm -r powerline-fonts.zip fonts-master
+
+curl -L https://codeload.github.com/gabrielelana/awesome-terminal-fonts/zip/v1.1.0 -o awesome-terminal-fonts.zip
+unzip awesome-terminal-fonts.zip -d awesome-terminal-fonts
+mv awesome-terminal-fonts/awesome-terminal-fonts-1.1.0/build/*.sh ~/.fonts/
+rm -r awesome-terminal-fonts*
 
 e_success "Done!"
