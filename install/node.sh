26dac22a408f79eef85d9f2299c15ce81e392232
Brandon Brown <brandon@brandonb.ca>
2017-10-03 12:13:05 -0700

    Update node.sh

--- install/node.sh
+++ install/node.sh
@@ -2,16 +2,13 @@
 
 source ./utils.sh
 
-e_header "Installing node, nvm, and npm"
-
-# For more info, see here https://gist.github.com/DanHerbert/9520689
-brew install node --without-npm
+e_header "Installing node nvm"
 
 curl -o- https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash
 . ~/.nvm/nvm.sh > /dev/null
-nvm install 6
-nvm alias stable 6
-nvm alias default 6
-nvm use 6
+nvm install 8
+nvm alias stable 8
+nvm alias default 8
+nvm use 8
 
 e_success "Done!"
