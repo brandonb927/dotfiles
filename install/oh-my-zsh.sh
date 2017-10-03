be950945b19287333f4da2140e53d4f31c1eb5cd
Brandon Brown <brandon@brandonb.ca>
2017-10-03 08:42:13 -0700

    Update oh-my-zsh.sh

--- install/oh-my-zsh.sh
+++ install/oh-my-zsh.sh
@@ -6,11 +6,4 @@ e_header "Installing oh-my-zsh"
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
-e_arrow "Installing oh-my-zsh custom plugins"
-
-mkdir -p ~/.oh-my-zsh/custom/plugins
-git clone git://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
-git clone git://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
-git clone git://github.com/lukechilds/zsh-nvm.git $ZSH_CUSTOM/plugins/zsh-nvm
-
 e_success "Done!"
