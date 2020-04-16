55fa002a49d3194c6324305aa14523130cc22b3b
Brandon Brown <brandon@brandonb.ca>
2020-04-15 23:01:42 -0700

    Update git.sh

--- install/git.sh
+++ install/git.sh
@@ -8,7 +8,7 @@ git config --global color.diff-highlight.oldNormal "red bold"
 git config --global color.diff-highlight.oldHighlight "red bold 52"
 git config --global color.diff-highlight.newNormal "green bold"
 git config --global color.diff-highlight.newHighlight "green bold 22"
-git config --global core.pager "diff-highlight | diff-so-fancy | less --tabs=1,5 -R"
+git config --global core.pager "diff-so-fancy | less --tabs=1,5 -R"
 
 # Create a nice last-change git log message, from https://twitter.com/elijahmanor/status/697055097356943360
 git config --global alias.lastchange 'log -p --follow -n 1'
