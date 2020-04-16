2d510b0ba7fe74315aba4a266f2705f0af76a3d7
Brandon Brown <brandon@brandonb.ca>
2020-04-15 22:59:58 -0700

    Update brew-cask.sh

--- install/brew-cask.sh
+++ install/brew-cask.sh
@@ -16,29 +16,25 @@ apps=(
   alfred
   appcleaner
   bartender
-  blackvue-viewer
-  brave-browser
   daisydisk
   discord
   docker
   expressvpn
   firefox
   flux
+  google-chrome
   homebrew/cask-drivers/steelseries-exactmouse-tool
-  # hajo-headphone-enhancer # Currently not working on macOS 10.13+, the driver doesn't install cleanly
   insomnia
   istat-menus
   iterm2
   java
   kap
   keybase
-  logitech-gaming-software
   logitech-options
   minecraft
   ngrok
   plex-media-player
   rectangle
-  skype
   slack
   spotify
   stay
@@ -48,7 +44,6 @@ apps=(
   visual-studio-code
   vlc
   webtorrent
-  zoomus
 )
 for app in "${apps[@]}"; do
   brew cask install $app
@@ -60,10 +55,7 @@ brew cask install qlcolorcode \
   qlstephen \
   qlmarkdown \
   quicklook-json \
-  qlprettypatch \
-  quicklook-csv \
   qlimagesize \
-  webpquicklook \
   suspicious-package \
   quicklookase \
   qlvideo
