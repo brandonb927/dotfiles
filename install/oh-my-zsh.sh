095ab2698f20d199bc06371e5ab478c85897e13d
Brandon Brown <brandon@brandonb.ca>
2020-04-15 23:07:26 -0700

    Update oh-my-zsh.sh

--- install/oh-my-zsh.sh
+++ install/oh-my-zsh.sh
@@ -5,6 +5,6 @@ source ./utils.sh
 e_header "Installing oh-my-zsh"
 e_arrow "NOTE: You'll need to restart this installation after installing oh-my zsh and the shell changes to zsh"
 
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 e_success "Done!"
