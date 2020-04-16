0963aade299a8218cc1ca31530996698a5d2e6f3
Brandon Brown <brandon@brandonb.ca>
2020-04-15 23:11:28 -0700

    Update ruby.sh

--- install/ruby.sh
+++ install/ruby.sh
@@ -4,11 +4,9 @@ source ./utils.sh
 
 e_header "Installing various versions of ruby"
 
-if $(command -v rbenv); then
-  rbenv install 2.4.2
-  rbenv install 2.5.1
-  rbenv install 2.6.3
-  rbenv global 2.6.3
+if type_exists 'rbenv'; then
+  rbenv install 2.6.5
+  rbenv global 2.6.5
 
   e_success "Done!"
 else
