3e814f819042f6d7aba9dd369ad029ef73cdad04
Brandon Brown <brandon@brandonb.ca>
2020-06-15 08:47:45 -0700

    Update mas.sh

--- install/mas.sh
+++ install/mas.sh
@@ -11,11 +11,11 @@ e_arrow "Installing Mac App Store apps" $cyan
 # Install this first because it will take the longest
 #mas install 497799835 # Xcode (9.4.1)
 
-#mas install 1091189122 # Bear (1.5.5)
-mas install 824183456 # Affinity Photo (1.6.7)
-mas install 425424353 # The Unarchiver (4.0.0)
+mas install 824183456  # Affinity Photo (1.6.7)
+mas install 425424353  # The Unarchiver (4.0.0)
 mas install 1294126402 # HEIC Converter (1.2.0)
-mas install 595191960 # CopyClip (1.9.3)
+mas install 595191960  # CopyClip (1.9.3)
+mas install 937984704  # Amphetamine (5.0.2)
 #mas install 682658836 # GarageBand (10.3.1)
 #mas install 408981434 # iMovie (10.1.9)
 
