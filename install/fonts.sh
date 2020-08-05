546f1072f022207c6c472583c327ff75c54dd5c3
Brandon Brown <brandon@brandonb.ca>
2020-08-05 13:53:41 -0700

    Update fonts.sh

--- install/fonts.sh
+++ install/fonts.sh
@@ -7,6 +7,10 @@ e_header "Installing fonts"
 brew tap homebrew/cask-fonts
 
 brew cask install \
-  font-hack-nerd-font
+  font-hack-nerd-font \
+  font-cascadia \
+  font-cascadia-mono \
+  font-cascadia-pl \
+  font-cascadia-mono-pl
 
 e_success "Done!"
