e25c27ba3d6fe7737621a1834ceae12122ce9b7b
Brandon Brown <brandon@brandonb.ca>
2022-07-25 15:21:35 -0700

    Update mas.sh

--- install/mas.sh
+++ install/mas.sh
@@ -9,15 +9,15 @@ brew install mas
 e_arrow "Installing Mac App Store apps" $cyan
 
 # Install this first because it will take the longest
-mas install 1215244636  YI Home         (1.1.9)
-mas install 1522267256  Shareful        (1.2.2)
-#mas install 595191960   CopyClip        (1.9.7)
-#mas install 408981434   iMovie          (10.2.2)
-mas install 937984704   Amphetamine     (5.1.1)
-mas install 1518036000  Sequel Ace      (3.2.2)
-mas install 824183456   Affinity Photo  (1.9.1)
-#mas install 1529006487  Recordia        (1.3.0)
-mas install 425424353   The Unarchiver  (4.2.4)
-mas install 1294126402  HEIC Converter  (1.9.0)
+#mas install 1215244636  # YI Home
+mas install 1522267256  # Shareful
+#mas install 408981434   # iMovie
+mas install 937984704   # Amphetamine
+#mas install 1518036000  # Sequel Ace
+#mas install 824183456   # Affinity Photo
+#mas install 1529006487  # Recordia
+mas install 425424353   # The Unarchiver
+mas install 1294126402  # HEIC Converter
+mas install 1507396839  # Abyss
 
 e_success "Done!"
