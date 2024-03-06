e115f830fe685d7e6f5ba3324d4f7a0912c83f38
Brandon Brown <brandon@brandonb.ca>
2024-03-06 15:22:03 -0800

    Update brew.sh

--- install/brew.sh
+++ install/brew.sh
@@ -4,8 +4,6 @@ source ./utils.sh
 
 e_header "Installing homebrew"
 
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)" \
-  && brew doctor \
-  && brew update
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)" && brew update
 
 e_success "Done!"
