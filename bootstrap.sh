1944893b45ce14ea7e40149ce4d6e28b943555a3
Brandon Brown <brandon@brandonb.ca>
2024-03-06 15:25:28 -0800

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -12,6 +12,27 @@ else
 fi
 
 echo ""
+echo "Install fish shell?  (y/n)"
+read -r response
+if [[ $response =~ ^([yY][eE][sS]|[yY])$ ]]; then
+  source ./install/fish.sh
+fi
+
+echo ""
+echo "Install asdf and languages/versions?  (y/n)"
+read -r response
+if [[ $response =~ ^([yY][eE][sS]|[yY])$ ]]; then
+  source ./install/asdf.sh
+fi
+
+echo ""
+echo "Install fonts?  (y/n)"
+read -r response
+if [[ $response =~ ^([yY][eE][sS]|[yY])$ ]]; then
+  source ./install/fonts.sh
+fi
+
+echo ""
 echo "Install brew utilities?  (y/n)"
 read -r response
 if [[ $response =~ ^([yY][eE][sS]|[yY])$ ]]; then
@@ -26,24 +47,11 @@ if [[ $response =~ ^([yY][eE][sS]|[yY])$ ]]; then
 fi
 
 echo ""
-echo "Install fonts?  (y/n)"
-read -r response
-if [[ $response =~ ^([yY][eE][sS]|[yY])$ ]]; then
-  source ./install/fonts.sh
-fi
-
-echo ""
 echo "Install mac app store apps?  (y/n)"
 read -r response
 if [[ $response =~ ^([yY][eE][sS]|[yY])$ ]]; then
+  brew install mas
   mas install 1522267256  # Shareful
   mas install 425424353   # The Unarchiver
   mas install 6444667067  # Hyperduck
-fi
-
-echo ""
-echo "Install asdf and languages/versions?  (y/n)"
-read -r response
-if [[ $response =~ ^([yY][eE][sS]|[yY])$ ]]; then
-  source ./install/asdf.sh
 fi
